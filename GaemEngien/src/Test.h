06a75822f98ccd83ca466d4ab2cb4c2768f54586
Zoinks <murarashuvlad@gmail.com>
2022-08-30 22:42:34 +0300

    Project setup

---
+++ GaemEngien/src/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace GaemEngien
+{
+	__declspec(dllexport) void Print();
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+namespace GaemEngien
+{
+	__declspec(dllimport) void Print();
+}
+
+void main()
+{
+	GaemEngien::Print();
+}
