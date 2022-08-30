929bf4b9b2fe766aa766fba6e814c42fdb2a7d36
Zoinks <murarashuvlad@gmail.com>
2022-08-31 02:12:52 +0300

    included premake and so now u can generate a project

---
+++ generateProjects.bat
@@ -0,0 +1,2 @@
+call vendor\bin\premake\bin\release\premake5.exe vs2022
+PAUSE
---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "GaemEngien"
+	architecture "x64"
+	startproject "Sandbox"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	
+project "GaemEngien"
+	location "GaemEngien"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include",
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"PLATFORM_WINDOWS",
+			"BUILD_DLL"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "DIST"
+		optimize "On"
+		
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"GaemEngien/vendor/spdlog/include",
+		"GaemEngien/src"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"PLATFORM_WINDOWS"
+		}
+
+		links
+		{
+			"GaemEngien"
+		}
+		
+	filter "configurations:Debug"
+		defines "DEBUG"
+		symbols "On"
+		
+	filter "configurations:Release"
+		defines "RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "DIST"
+		optimize "On"
