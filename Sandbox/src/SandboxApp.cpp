9e1d9907f13075b9ec309a16e76284f5026fa33c
Zoinks <murarashuvlad@gmail.com>
2022-09-14 20:20:47 +0300

    we now have the concept of transforms and we made a lil grid

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,46 +2,52 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public MidnightEngine::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
 	{
-		m_VertexArray.reset(MidnightEngine::VertexArray::Create());
+		// Rendering T R Y E N G U L
 
-		float vertices[3 * 7] = {
+
+		m_TriangleVertexArray.reset(MidnightEngine::VertexArray::Create());
+
+		float triangleVertices[3 * 7] = {
 			-0.5f, -0.5f, 0.0f, 0.9f, 0.7f, 1.0f, 1.0f,
 			 0.5f, -0.5f, 0.0f, 0.5f, 0.0f, 1.0f, 1.0f,
 			 0.0f,  0.5f, 0.0f, 0.0f, 0.9f, 0.2f, 1.0f,
 		};
 
-		std::shared_ptr<MidnightEngine::VertexBuffer> vertexBuffer;
+		std::shared_ptr<MidnightEngine::VertexBuffer> triangleVertexBuffer;
 
-		vertexBuffer.reset(MidnightEngine::VertexBuffer::Create(vertices, sizeof(vertices)));
+		triangleVertexBuffer.reset(MidnightEngine::VertexBuffer::Create(triangleVertices, sizeof(triangleVertices)));
 
-		MidnightEngine::BufferLayout layout = {
+		MidnightEngine::BufferLayout triangleLayout = {
 			{MidnightEngine::ShaderDataType::Float3, "a_Position"},
 			{MidnightEngine::ShaderDataType::Float4, "a_Color"},
 		};
 
-		vertexBuffer->SetLayout(layout);
-		m_VertexArray->AddVertexBuffer(vertexBuffer);
+		triangleVertexBuffer->SetLayout(triangleLayout);
+		m_TriangleVertexArray->AddVertexBuffer(triangleVertexBuffer);
 
-		uint32_t indices[3] = { 0, 1, 2 };
+		uint32_t triangleIndices[3] = { 0, 1, 2 };
 
-		std::shared_ptr<MidnightEngine::IndexBuffer> indexBuffer;
+		std::shared_ptr<MidnightEngine::IndexBuffer> triangleIndexBuffer;
 
-		indexBuffer.reset(MidnightEngine::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-		m_VertexArray->SetIndexBuffer(indexBuffer);
+		triangleIndexBuffer.reset(MidnightEngine::IndexBuffer::Create(triangleIndices, sizeof(triangleIndices) / sizeof(uint32_t)));
+		m_TriangleVertexArray->SetIndexBuffer(triangleIndexBuffer);
 
-		std::string vertexSrc = R"(
+		std::string triangleVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
 			layout(location = 1) in vec4 a_Color;
 			
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -50,11 +56,11 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
-		std::string fragmentSrc = R"(
+		std::string triangleFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
@@ -68,7 +74,77 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new MidnightEngine::Shader(vertexSrc, fragmentSrc));
+		m_TriangleShader.reset(new MidnightEngine::Shader(triangleVertexSrc, triangleFragmentSrc));
+
+
+
+
+
+
+
+
+
+
+		// Rendering S K U E R
+
+		m_SquareVertexArray.reset(MidnightEngine::VertexArray::Create());
+
+		float squareVertices[4 * 3] = {
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f,
+		};
+
+		std::shared_ptr<MidnightEngine::VertexBuffer> squareVertexBuffer;
+
+		squareVertexBuffer.reset(MidnightEngine::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+
+		MidnightEngine::BufferLayout squareLayout = {
+			{MidnightEngine::ShaderDataType::Float3, "a_Position"},
+		};
+
+		squareVertexBuffer->SetLayout(squareLayout);
+		m_SquareVertexArray->AddVertexBuffer(squareVertexBuffer);
+
+		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+
+		std::shared_ptr<MidnightEngine::IndexBuffer> squareIndexBuffer;
+
+		squareIndexBuffer.reset(MidnightEngine::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVertexArray->SetIndexBuffer(squareIndexBuffer);
+
+		std::string sqaureVertexSrc = R"(
+			#version 330 core
+
+			layout(location = 0) in vec3 a_Position;
+			
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
+			out vec3 v_Position;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string squareFragmentSrc = R"(
+			#version 330 core
+
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
+		m_SquareShader.reset(new MidnightEngine::Shader(sqaureVertexSrc, squareFragmentSrc));
 	}
 
 	void OnUpdate(MidnightEngine::Timestep ts) override
@@ -111,7 +187,19 @@ public:
 
 		MidnightEngine::Renderer::BeginScene(m_Camera);
 
-		MidnightEngine::Renderer::Submit(m_Shader, m_VertexArray);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (size_t y = 0; y < 20; y++)
+		{
+			for (size_t x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11, 0.0);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				MidnightEngine::Renderer::Submit(m_SquareShader, m_SquareVertexArray, transform);
+			}
+		}
+
+		MidnightEngine::Renderer::Submit(m_TriangleShader, m_TriangleVertexArray);
 
 		MidnightEngine::Renderer::EndScene();
 	}
@@ -121,8 +209,11 @@ public:
 	void OnEvent(MidnightEngine::Event& event) override {}
 
 private:
-	std::shared_ptr<MidnightEngine::Shader> m_Shader;
-	std::shared_ptr<MidnightEngine::VertexArray> m_VertexArray;
+	std::shared_ptr<MidnightEngine::Shader> m_TriangleShader;
+	std::shared_ptr<MidnightEngine::VertexArray> m_TriangleVertexArray;
+
+	std::shared_ptr<MidnightEngine::Shader> m_SquareShader;
+	std::shared_ptr<MidnightEngine::VertexArray> m_SquareVertexArray;
 
 	MidnightEngine::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
